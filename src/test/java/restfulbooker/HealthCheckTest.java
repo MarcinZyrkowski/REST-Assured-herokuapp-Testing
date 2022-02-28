2356101e5b7e764d083b08106ec1635e6bfd75cd
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-02-28 12:16:50 +0100

    added old school test's approach, and implement BDD style

--- src/test/java/restfulbooker/HealthCheckTest.java
+++ src/test/java/restfulbooker/HealthCheckTest.java
@@ -1,4 +1,19 @@
 package restfulbooker;
 
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+
 public class HealthCheckTest {
+
+    @Test
+    public void healthCheckTest(){
+        given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/ping")
+                .then()
+                .assertThat()
+                .statusCode(201);
+    }
+
 }
