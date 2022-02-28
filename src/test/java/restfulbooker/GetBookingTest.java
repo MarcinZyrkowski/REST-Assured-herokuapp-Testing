b0f0048ba86d83600d261bb4113211ff7fc5c069
mzyrkowski <marcinzyrkowski7@gmail.com>
2022-02-28 18:29:16 +0100

    added put test

---
+++ src/test/java/restfulbooker/GetBookingTest.java
@@ -0,0 +1,48 @@
+package restfulbooker;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class GetBookingTest {
+
+    @Test
+    public void getBookingTest() {
+        // Get response with booking
+        Response response = RestAssured.get("https://restful-booker.herokuapp.com/booking/5");
+        response.print();
+
+        // Verify response 200
+        Assert.assertEquals(response.getStatusCode(), 200, "Status code should be 200, but it's not");
+
+        // Verify All fields
+        SoftAssert softAssert = new SoftAssert();
+        String actualFirstName = response.jsonPath().getString("firstname");
+        softAssert.assertEquals(actualFirstName, "Sally", "firstname in response is not expected");
+
+        String actualLastName = response.jsonPath().getString("lastname");
+        softAssert.assertEquals(actualLastName, "Ericsson", "lastname in response is not expected");
+
+        int price = response.jsonPath().getInt("totalprice");
+        softAssert.assertEquals(price, 753, "totalprice in response is not expected");
+
+        boolean depositpaid = response.jsonPath().getBoolean("depositpaid");
+        softAssert.assertTrue(depositpaid, "depositpaid should be true, but it's not");
+
+        String actualCheckin = response.jsonPath().getString("bookingdates.checkin");
+        softAssert.assertEquals(actualCheckin, "2016-02-06", "checkin in response is not expected");
+
+        String actualCheckout = response.jsonPath().getString("bookingdates.checkout");
+        softAssert.assertEquals(actualCheckout, "2016-09-27", "checkout in response is not expected");
+
+        String actualAdditionalneeds = response.jsonPath().getString("additionalneeds");
+        softAssert.assertEquals(actualAdditionalneeds, "Breakfast", "additionalneeds in response is not expected");
+
+        // assertAll method executes all above soft assertions
+        // it is disabled because API tested here is public and between test executions everyone can change data and makes test failed
+        // softAssert.assertAll();
+    }
+
+}
